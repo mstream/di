2ab50fe73494a5e869b6be726e32a468681b20b0
mstream <maciej.laciak@gmail.com>
2023-01-17 22:34:15 +0000

    style: rewrite test to use chained invocation style

--- test/index.test.js
+++ test/index.test.js
@@ -3,46 +3,54 @@ import { createContainer } from "../src";
 
 describe(`createContainer()`, () => {
   it(`allows to register and invoke creators`, () => {
-    const container = createContainer();
-    container.register(`string`, () => `abc`);
-    expect(container.context.string).toBe(`abc`);
+    const { string } = createContainer()
+      .register(`string`, () => `abc`)
+      .context;
+
+    expect(string).toBe(`abc`);
   });
   it(`does not call a creator until requested`, () => {
-    const container = createContainer();
-    container.register(`error`, () => {
-      throw Error();
-    });
-    container.register(`string`, () => `abc`);
-    expect(container.context.string).toBe(`abc`);
+    const { string } = createContainer()
+      .register(`error`, () => {
+        throw Error();
+      })
+      .register(`string`, () => `abc`)
+      .context;
+
+    expect(string).toBe(`abc`);
   });
   it(`allow registring creators in any order`, () => {
-    const container = createContainer();
-    container.register(`string`, ({ number }) => `abc${number}`);
-    container.register(`number`, () => 1);
-    expect(container.context.string).toBe(`abc1`);
+    const { string } = createContainer()
+      .register(`string`, ({ number }) => `abc${number}`)
+      .register(`number`, () => 1)
+      .context;
+
+    expect(string).toBe(`abc1`);
   });
   it(`fails when creator is not a function`, () => {
-    const container = createContainer();
-    expect(() => container.register(`string`, `abc`)).toThrow(
-      /"string" creator is not a function/
-    );
+    expect(() => createContainer()
+      .register(`string`, `abc`)
+    )
+      .toThrow(
+        /"string" creator is not a function/
+      );
   });
   it(`prevents from overriding creators`, () => {
-    const container = createContainer();
-    container.register(`string`, () => `abc`);
+    const container = createContainer()
+      .register(`string`, () => `abc`);
     expect(() => container.register(`string`, () => `def`)).toThrow(
       /"string" is already registered in the context/
     );
   });
   it(`does not execute creators more than once`, () => {
     let counter = 0;
-    const container = createContainer();
-    container.register(`string`, () => {
-      counter += 1;
-      return `abc`;
-    });
-    container.context.string;
-    expect(container.context.string).toBe(`abc`);
+    const { context } = createContainer()
+      .register(`string`, () => {
+        counter += 1;
+        return `abc`;
+      });
+    context.string;
+    expect(context.string).toBe(`abc`);
     expect(counter).toBe(1);
   });
 });
