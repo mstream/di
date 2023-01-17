cc5cdbb793916b8533ccf39df12e1597fb7b5383
mstream <maciej.laciak@gmail.com>
2023-01-17 17:33:30 +0000

    docs: add more unit test to the ref app

---
+++ example/test/app.test.js
@@ -0,0 +1,20 @@
+import sinon from "sinon";
+import { describe, expect, it } from "vitest";
+import appCreator from "../src/app.js";
+
+describe(`app`, () => {
+  it(`logs a message at every possible level`, () => {
+    const debug = sinon.spy();
+    const error = sinon.spy();
+    const info = sinon.spy();
+    const warning = sinon.spy();
+    appCreator({
+      logger: { debug, error, info, warning },
+    })(`abc`);
+
+    expect(debug.calledWith(`abc`)).toBe(true);
+    expect(error.calledWith(`abc`)).toBe(true);
+    expect(info.calledWith(`abc`)).toBe(true);
+    expect(warning.calledWith(`abc`)).toBe(true);
+  });
+});
